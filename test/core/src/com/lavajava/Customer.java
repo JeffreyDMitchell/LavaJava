9ee57361e46ab9a39717036790a5ff04b30d09e5
Kaitlyn Huynh <kahu1074@colorado.edu>
2023-04-19 20:32:46 -0600

    added customer class

---
+++ test/core/src/com/lavajava/Customer.java
@@ -0,0 +1,21 @@
+package com.lavajava;
+
+import com.badlogic.gdx.Gdx;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+public class Customer{
+    int patience;
+    Drink request;
+
+    public evalDrink(Drink d){
+        
+    }
+}
